851863ab8fcdc1c57668b4287a6ea9f493066fe7
ParthaMalakar <parthamalakar4321@gmail.com>
2023-12-22 02:15:41 +0600

    dashboard picture added

--- src/Components/RootPage/Dashboard.jsx
+++ src/Components/RootPage/Dashboard.jsx
@@ -1,11 +1,14 @@
 import { FaAd, FaBook, FaCalendar, FaEnvelope, FaHome, FaList, FaMarkdown, FaSearch, FaShoppingCart, FaStreetView, FaUsers, FaUtensils } from "react-icons/fa";
-import { NavLink, Outlet } from "react-router-dom";
+import { Link, NavLink, Outlet } from "react-router-dom";
 
 import { MdOutlineClearAll } from "react-icons/md";
 import { TbPlaystationSquare } from "react-icons/tb";
+import { useContext } from "react";
+import { AuthContext } from "../../provider/Authprovider";
 
 const Dashboard = () => {
 
+    const { user } = useContext(AuthContext);
 
     return (
         <div>
@@ -14,6 +17,20 @@ const Dashboard = () => {
                 {/* dashboard side bar */}
                 <div className="md:w-64 md:min-h-screen bg-amber-300">
                     <ul className="menu p-4">
+                    {
+                        user ? <div className=" md:flex items-center w-[250px] ">
+                            <div className="dropdown">
+                                <label tabIndex={0} className="btn btn-ghost btn-circle w-[200px]">
+                                <img  className="ml-40 md:ml-0 h-[120px]   " src={user.photoURL} alt="" />
+                                </label>
+                               
+                            </div>
+
+                        </div>
+
+                            :
+                            <Link to="/login" className="py-2 px-6 rounded-lg  bg-slate-600 text-white text-lg font-semibold mr-10">Login</Link>
+                    }
                         {
                            
                             <>
